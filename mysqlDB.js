b7e65417376320e3ba85af2c5263eb1dd878cced
wnsvy <wnsvy223@naver.com>
2018-12-29 17:16:23 +0900

    mysql config

--- mysqlDB.js
+++ mysqlDB.js
@@ -2,7 +2,6 @@ var mysql = require('mysql');
 /*
 var connection = mysql.createConnection({
     host : "192.168.35.67", //우리집
-    //host : "192.168.0.11", //승진이집
     port : 3306, //mysql db port
     user : "root",
     password: "root",
@@ -12,8 +11,6 @@ var connection = mysql.createConnection({
 */
 var pool = mysql.createPool({
     host : "192.168.0.29", // 우리집
-    //host : "192.168.0.11", //승진이집
-    //host : "175.119.217.23", // 성남집 공인IP
     port : 3306, //mysql db port
     user : "root",
     password: "root",
