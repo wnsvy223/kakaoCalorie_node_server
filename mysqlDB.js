59a22665f1d25017a422eb4077ac283161c156bc
wnsvy <wnsvy223@naver.com>
2019-04-17 19:00:41 +0900

    remove mysql mutipleStatments option

--- mysqlDB.js
+++ mysqlDB.js
@@ -17,7 +17,8 @@ var pool = mysql.createPool({
     database : "kakaocalorie",
     connectionLimit : 50,
     charset: 'utf8',
-    multipleStatements : true // 다중쿼리
+    //multipleStatements : true 
+    // 다중쿼리 가능하게 해주는 옵션 => 다중쿼리 옵션을 켤경우 sql 인젝션 공격을 가능해짐. 방어수단을 따로 두지 않는 이상 true옵션 위험.
 });
 
 module.exports = pool;
